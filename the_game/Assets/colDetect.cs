17fc32a46f87695911b8b30ca36074dd1e9a27a3
viliusbankauskas <bankauskasvil@gmail.com>
2022-04-27 12:56:01 +0300

    APES-00

--- the_game/Assets/colDetect.cs
+++ the_game/Assets/colDetect.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 public class colDetect : MonoBehaviour
 {
     public Text score;
-    private float Score = 0;
+    public Text money;
+    float Score = 0;
+    float Money = 0;
 
     void Update()
     {
         score.text = "Points: " + Score.ToString();
+        money.text = "Money: " + Money.ToString();
     }
     //public Text score;
     // Start is called before the first frame update
     private void OnTriggerEnter2D (Collider2D other)
     {
-        Score = Score + 50;
+        Score += 50;
+        Money += 25;
         other.gameObject.SetActive(false);
     }
 }
